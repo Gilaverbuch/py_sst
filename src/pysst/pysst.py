2f4527934ea901faae8e80ce1c4d31ada0ecfdb3
Gilaverbuch <gilaverbuch@gmail.com>
2023-07-28 15:02:21 -0400

    added sst class functionality

--- src/pysst/pysst.py
+++ src/pysst/pysst.py
@@ -49,3 +49,53 @@ def load_data(date_i, date_f = None, merge = True):
     return sst
 
 
+
+class SST(object):
+    def __init__(self, date_i = None, date_f = None):
+
+        self.date_i = date_i
+        self.date_f = date_f
+
+
+    def load_data(self):
+        '''
+        this function loads satelite SST data and returns an xarray object with the data. 
+        
+        parameters
+        ----------
+            date_i: initial time as numpy datetime64 object or array of datetime64 objects
+            date_f: Default is None unless final time is given as numpy datetime64 object or array of datetime64 objects. 
+            merge: merge data sets from various sources. Default is True.
+
+        Returns
+        -------
+        xarray object
+        '''
+        
+        self.sst = load_data_(self.date_i, self.date_f)
+
+        # if merge is True:
+        #     sst = merge_data_(sst)
+
+        # return sst
+
+
+    def merge(self):
+        '''
+        this function merges sst data from both datasets 
+        
+        parameters
+        ----------
+
+
+        Returns
+        -------
+        xarray object
+        '''
+        
+        self.sst = merge_data_(self.sst)
+
+
+
+
+
